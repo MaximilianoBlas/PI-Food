e9b4eb4d402b3f2c37f795f56a59c40fd4390bef
Maximiliano Blas Silva <silvamaxiblas@gmail.com>
2022-08-27 19:34:55 -0300

    change api index

--- api/index.js
+++ api/index.js
@@ -17,13 +17,13 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const {createDiets} =  require("./src/dbinstance/dbinstance.js");
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const { createDiets } = require("./src/dbinstance/dbinstance.js");
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  createDiets();
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
+conn.sync({ force: false }).then(() => {
+    createDiets();
+    server.listen(process.env.PORT, () => {
+        console.log("%s listening at 3001"); // eslint-disable-line no-console
+    });
 });
